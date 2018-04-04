e073410bddf6971e5eeee2ee94769ba27925b9bc
William Zeng <william.zeng@macaulay.cuny.edu>
2018-04-03 20:26:19 -0400

    Create euler36(4.3).py

---
+++ euler36(4.3).py
@@ -0,0 +1,32 @@
+""""The decimal number, 585 = 10010010012 (binary), is palindromic in both bases. Find all numbers under 1 million that are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)""""
+
+
+def conv_bin(number): # write function to convert to bin
+    return format(number,'b')
+
+def is_pal(string): # write function to convert to palin
+    if string == string[::-1]:
+        return True #boolean
+    else:
+        return False
+
+def main():
+    total = 0 #initalize total
+    for i in range (0,1000000): #range under million
+        # find first value in base 10, no need to convert
+        # if palindromic will return boolean true
+        val = is_pal(str(i)) 
+        #convert using fuction 
+        binary = conv_bin(i)
+        # find second value in base 2,
+        # if palindromic will return boolean true
+        val2 = is_pal(str(binary))
+        #compare
+        if val == True and val2 == True:
+            #sum
+            total = total + i
+    print (total)
+    
+main()
